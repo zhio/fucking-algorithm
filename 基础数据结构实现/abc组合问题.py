a009f66f05085eeebb4b169be49a6854ab9a37fc
zhio <gpfer@qq.com>
2021-07-19 11:47:11 +0800

    二分查找

--- 基础数据结构实现/abc组合问题.py
+++ 基础数据结构实现/abc组合问题.py
@@ -1,23 +1,29 @@
-# -*- coding:utf-8 -*-
-__author__ = 'ChenJiaBao'
-__date__ = '2019/2/9 12:17'
+import time
 # 如果 a+b+c=1000,且a^2+b^2=c^2(a,b,c为自然数)，如何求出所有的a,b,c可能的组合
 
 
 # 效率低下算法 时间复杂度为 O(n^3)
-# for a in range(0, 1001):
-#     for b in range(0, 1001):
-#         for c in range(0, 1001):
-#             if (a + b + c == 1000) and (a**2+b**2 == c**2):
-#                 print("a,b,c=", a, b, c)
+def abc01():
+    start_time = time.time()
+    for a in range(0, 1001):
+        for b in range(0, 1001):
+            for c in range(0, 1001):
+                if (a + b + c == 1000) and (a**2+b**2 == c**2):
+                    print("a,b,c=", a, b, c)
 
+    end_time = time.time()
+    print(f"花费时间为{end_time-start_time}")
 
 # 改进算法 时间复杂度为 O(n^2)
-for a in range(0, 1001):
-    for b in range(0, 1001):
-        c = 1000 - a - b
-        if a**2 + b**2 == c**2:
-            print('a,b,c=', a, b, c)
+def abc02():
+    start_time = time.time()
+    for a in range(0, 1001):
+        for b in range(0, 1001):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                print('a,b,c=', a, b, c)
+    end_time = time.time()
+    print(f"花费时间为{end_time-start_time}")
 
 
 # 时间复杂度计算规则
@@ -30,3 +36,6 @@ for a in range(0, 1001):
 时间复杂度大小关系：
 O(1)<O(logn)<O(n)<O(nlogn)<O(n^2)<O(n^3)<O(2^n)<O(n!)<O(n^n)
 """
+if __name__ == '__main__':
+    abc01()
+    abc02()
